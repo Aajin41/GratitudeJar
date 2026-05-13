27ebddbabe6203eb01ba092ca2afdf56a2c8085a
Rommel <24-07945@g.batstate-u.edu.ph>
2026-05-13 20:27:34 +0800

    Delete JarAppConsoleVersion directory

--- JarAppConsoleVersion/JarTier.cs
+++
@@ -1,1 +0,0 @@
-public enum JarTier { Glass, Gold, Diamond }
--- JarAppConsoleVersion/MemoryEntry.cs
+++
@@ -1,6 +0,0 @@
-public class MemoryEntry : Entry
-{
-    public required string Image { get; set; }
-    public override void Display() => Console.WriteLine($"Memory: {Content} (Image: {Image})");
-    public override string Summarize() => $"Memory: {Content}";
-}
